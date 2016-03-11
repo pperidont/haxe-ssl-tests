aa06c6547f24a64f775f658cd39908efa5fa7f07
Pascal Peridont <pperidont@motion-twin.com>
2016-03-11 09:07:29 +0100

    let std lib load default CA

--- src/TestHttp.hx
+++ src/TestHttp.hx
@@ -1,8 +1,6 @@
 class TestHttp {
 
 	public function new() {
-		sys.ssl.Socket.DEFAULT_CA = sys.ssl.Certificate.loadFile("cert/global.cer");
-		
 		var h = new haxe.Http("https://google.com");
 		h.onData = function(d){
 			Sys.print( d );
